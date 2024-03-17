fa8befbc4c0e38bc1d295abad2a57e2884247a89
Edgar Benavides <edgarben27@gmail.com>
2024-03-16 21:54:43 -0600

    feat: add prisma configuration

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function excludeFields(entity: any, keys: string[]) {
+  const clone = { ...entity };
+
+  keys.forEach((key) => {
+    if (clone[key]) {
+      delete clone[key];
+    }
+  });
+
+  return clone;
 }
